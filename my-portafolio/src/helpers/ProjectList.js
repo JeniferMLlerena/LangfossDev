29cb5e6a253213d507f61ad7350a979a0dade69f
Jenifer Langfoss <jen.langfoss@gmail.com>
2023-05-09 00:43:29 -0600

    added PROJECT LIST helper and edited Projects.js

---
+++ my-portafolio/src/helpers/ProjectList.js
@@ -0,0 +1,14 @@
+import Proj1 from "../assets/HHT-Homepage.png";
+
+export const ProjectList = [
+  {
+    name: "Dijkstra Algorithm Visualizer",
+    image: Proj1,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "Ecommerce Website",
+    image: Proj1,
+    skills: "React,Node.js,MongoDB",
+  },
+];
