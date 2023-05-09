eb07729e5d1d93b1f6a697039ab67d3d39fc20fb
Jenifer Langfoss <jen.langfoss@gmail.com>
2023-05-09 01:17:27 -0600

    added project display and css

--- my-portafolio/src/App.js
+++ my-portafolio/src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Experience from './pages/Experience';
-import Projects from './pages/Projects';
-import NavBar from './component/NavBar';
-import Footer from './component/Footer';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Experience from "./pages/Experience";
+import Projects from "./pages/Projects";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import ProjectDisplay from "./pages/ProjectDisplay";
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path="/experience" element={<Experience />} />
         </Routes>
         <Footer />
