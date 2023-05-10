9d7e8651ce1814ecd29b64bd8089e02d8c272aa9
Jenifer Langfoss <jen.langfoss@gmail.com>
2023-05-10 03:45:26 -0600

    finished front-end icons

--- my-portafolio/src/pages/Home.js
+++ my-portafolio/src/pages/Home.js
@@ -27,21 +27,33 @@ function Home() {
               <div class="skillIcon"><img src={require("../assets/html-5.png")} alt="html-5"/><div>HTML</div></div>
               <div class="skillIcon"><img src={require("../assets/css3.png")} alt="css3"/><div>CSS3</div></div>
               <div class="skillIcon"><img src={require("../assets/sass.png")} alt="sass"/><div>SASS</div></div>
-              <div class="skillIcon"><img src={require("../assets/javascript.png")} alt="javascript"/><div>Javascript</div></div>
-              <div class="skillIcon"><img src={require("../assets/typescript.png")} alt="typescript"/><div>Typescript</div></div>
-              <div class="skillIcon"><img src={require("../assets/jquery.png")} alt="jquery"/><div>JQuery</div></div>
-              <div class="skillIcon"><img src={require("../assets/rest-api.png")} alt="rest-api"/><div>Rest API</div></div>
+              <div class="skillIcon"><img src={require("../assets/jquery.png")} alt="jquery"/><div>jQuery</div></div>
               <div class="skillIcon"><img src={require("../assets/git.png")} alt="git"/><div>GIT</div></div>
               <div class="skillIcon"><img src={require("../assets/github.png")} alt="github"/><div>Github</div></div>
               <div class="skillIcon"><img src={require("../assets/react-native.png")} alt="react-native"/><div>React</div></div>
               <div class="skillIcon"><img src={require("../assets/angularjs.png")} alt="angularjs"/><div>Angular</div></div>
+              <div class="skillIcon"><img src={require("../assets/rest-api.png")} alt="rest-api"/><div>Rest API</div></div>
+              <div class="skillIcon"><img src={require("../assets/javascript.png")} alt="javascript"/><div>Javascript</div></div>
+              <div class="skillIcon"><img src={require("../assets/typescript.png")} alt="typescript"/><div>Typescript</div></div>
+              <div class="skillIcon"><img src={require("../assets/woocommerce.png")} alt="woocommerce "/><div>WooCommerce</div></div>
               <div class="skillIcon"><img src={require("../assets/w3c.png")} alt="w3c "/><div>W3C</div></div>
-              <div class="skillIcon"><img src={require("../assets/web-accessibility.png")} alt="web-accessibility "/><div>Web Accessibility</div></div>
+              <div class="skillIcon"><img src={require("../assets/wordpress.png")} alt="wordpress "/><div>Wordpress</div></div>
+              <div class="skillIcon"><img src={require("../assets/console.png")} alt="console"/><div>Command Line</div></div>
+              <div class="skillIcon"><img src={require("../assets/performance.png")} alt="web-performance"/><div>Web Performance</div></div>
+              <div class="skillIcon"><img src={require("../assets/web-accessibility.png")} alt="web-accessibility"/><div>Web Accessibility</div></div>
               <div class="skillIcon"><img src={require("../assets/web-design.png")} alt="web-design "/><div>Responsive Web Design </div></div>
             </div>
           </li>
           <li className="item">
-            <h2>Back-End</h2>
+            <h2>Back-End</h2>            
+            <div class="backend">
+              <div class="skillIcon"><img src={require("../assets/html-5.png")} alt="NodeJS"/><div>NodeJS</div></div>
+              <div class="skillIcon"><img src={require("../assets/html-5.png")} alt="MongoDB"/><div>MongoDB</div></div>
+              <div class="skillIcon"><img src={require("../assets/html-5.png")} alt="PostgreSQL"/><div>PostgreSQL</div></div>
+              <div class="skillIcon"><img src={require("../assets/css3.png")} alt="ExpressJS"/><div>ExpressJS</div></div>
+              <div class="skillIcon"><img src={require("../assets/sass.png")} alt="MySQL"/><div>MySQL</div></div>
+              <div class="skillIcon"><img src={require("../assets/jquery.png")} alt="AWS S3"/><div>AWS S3</div></div>
+            </div>
             <span>
               NodeJS, Java Spring, .NET, ExpressJS, GraphQL, ApolloServer,
               MySQL, MongoDB, DynamoDB, DigitalOcean, AWS S3, MS SQL
