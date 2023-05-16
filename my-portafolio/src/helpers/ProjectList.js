7ae56606dbe23fabc8ad4acd4bc1ed2e0a190d64
Jenifer Langfoss <jen.langfoss@gmail.com>
2023-05-15 22:48:21 -0600

    adding images for projects

--- my-portafolio/src/helpers/ProjectList.js
+++ my-portafolio/src/helpers/ProjectList.js
@@ -1,34 +1,39 @@
 import Proj1 from "../assets/HHT-mockup.png";
+import Proj2 from "../assets/SP-mockup.png";
+import Proj3 from "../assets/AG-mockup.png";
+import Proj4 from "../assets/ScissorLift-mockup.png";
+import Proj5 from "../assets/SkidSteer-mockup.png";
+import Proj6 from "../assets/trainingosha-mockup.png";
 
 export const ProjectList = [
   {
-    name: "Dijkstra Algorithm Visualizer",
+    name: "E-commerce hardhattraining.com",
     image: Proj1,
-    skills: "JavaScript,HTML,CSS",
+    skills: "JavaScript | HTML | CSS",
   },
   {
-    name: "Ecommerce Websiteddddddddddddd dddddddd",
-    image: Proj1,
-    skills: "React,Node.js,MongoDB",
+    name: "E-commerce safetyprovisions.com",
+    image: Proj2,
+    skills: "JavaScript | HTML | CSS",
   },
   {
-    name: "Ecommerce Websiteddddddddddddd dddddddd",
-    image: Proj1,
-    skills: "React,Node.js,MongoDB",
+    name: "E-commerce agsafetytraining.com",
+    image: Proj3,
+    skills: "JavaScript | HTML | CSS",
   },
   {
-    name: "Ecommerce Websiteddddddddddddd dddddddd",
-    image: Proj1,
-    skills: "React,Node.js,MongoDB",
+    name: "E-commerce Scissorliftsafetytraining.com",
+    image: Proj4,
+    skills: "JavaScript | HTML | CSS",
   },
   {
-    name: "Ecommerce Websiteddddddddddddd dddddddd",
-    image: Proj1,
-    skills: "React,Node.js,MongoDB",
+    name: "E-commerce Skidsteersafetytraining.com",
+    image: Proj5,
+    skills: "JavaScript | HTML | CSS",
   },
   {
-    name: "Ecommerce Websiteddddddddddddd dddddddd",
-    image: Proj1,
-    skills: "React,Node.js,MongoDB",
+    name: "E-commerce training-osha.com",
+    image: Proj6,
+    skills: "JavaScript | HTML | CSS",
   },
 ];
--- my-portafolio/src/pages/Projects.js
+++ my-portafolio/src/pages/Projects.js
@@ -10,7 +10,7 @@ function Projects() {
       <div className="projectList">
         {ProjectList.map((project, index) => {
           return (
-            <ProjectItem id={index} name={project.name} image={project.image} />
+            <ProjectItem id={index} name={project.name} image={project.image} skills={project.skills}/>
           );
         })}
       </div>
